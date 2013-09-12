ee346c00e2875a07698fea20d5ab1140e0f044d8
Yanghai Zhang <zhang9560@outlook.com>
2013-09-12 08:00:46 +0800

    Use TabPagerIndicator

--- src/app/weight/tracker/MainActivity.java
+++ src/app/weight/tracker/MainActivity.java
@@ -3,6 +3,7 @@ package app.weight.tracker;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import com.viewpagerindicator.TabPageIndicator;
 
 public class MainActivity extends Activity {
     @Override
@@ -13,6 +14,9 @@ public class MainActivity extends Activity {
         mViewPager = (ViewPager)findViewById(R.id.view_pager);
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getFragmentManager(), this);
         mViewPager.setAdapter(viewPagerAdapter);
+
+        TabPageIndicator pageIndicator = (TabPageIndicator)findViewById(R.id.page_indicator);
+        pageIndicator.setViewPager(mViewPager);
     }
 
     private ViewPager mViewPager;
