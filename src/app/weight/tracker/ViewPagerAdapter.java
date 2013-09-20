082d60681b51b2b56c0cfa32aba0d877065fd95f
Yanghai Zhang <zhang9560@outlook.com>
2013-09-20 22:44:05 +0800

    Add chinese translation

--- src/app/weight/tracker/ViewPagerAdapter.java
+++ src/app/weight/tracker/ViewPagerAdapter.java
@@ -6,10 +6,14 @@ import android.content.Context;
 import android.support.v13.app.FragmentPagerAdapter;
 
 public class ViewPagerAdapter extends FragmentPagerAdapter {
-    private static final String[] TITLES = new String[] {"WEIGHT LIST", "LINE CHART", "SETTINGS"};
+    private static final String[] TITLES = new String[3];
 
     public ViewPagerAdapter(FragmentManager fm, Context context) {
         super(fm);
+
+        TITLES[0] = context.getString(R.string.weight_list);
+        TITLES[1] = context.getString(R.string.line_chart);
+        TITLES[2] = context.getString(R.string.settings);
 
         mFragments = new Fragment[3];
         mFragments[0] = Fragment.instantiate(context, WeightListFragment.class.getName(), null);
